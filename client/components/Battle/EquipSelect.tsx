298fdba2f63178bc00f2b4dd9fd136d1574cfa07
Caleb Matthews <calebjmatthews@gmail.com>
2025-11-01 15:50:36 -0500

    feat: disable equipment when !canSelect

--- client/components/Battle/EquipSelect.tsx
+++ client/components/Battle/EquipSelect.tsx
@@ -5,6 +5,7 @@ import type BattleState from "@common/models/battleState";
 import Fighter from "@common/models/fighter";
 import equipments from '@common/instances/equipments';
 import { EQUIPMENT_SLOTS } from "@common/enums";
+import type Equipment from "@common/models/equipment";
 
 export default function EquipSelect(props: {
   battleState: BattleState,
@@ -35,11 +36,13 @@ export default function EquipSelect(props: {
     <section id="equip-select">
 
       {topEquip && (
-        <div className="select-panel top-bottom-select">
-          <div className="text-large">{topEquip.id}</div>
-          <div className="select-description">{topEquip.description}</div>
-          <button onClick={() => setEquipSelected(topEquip.id)}>{`Use`}</button>
-        </div>
+        <EquipSelectPanel
+          equip={topEquip}
+          isTopBottom={true}
+          setEquipSelected={setEquipSelected}
+          battleState={battleState}
+          toCommand={toCommand}
+        />
       )}
       {!topEquip && (
         <div className="select-panel select-empty top-bottom-select">
@@ -52,11 +55,13 @@ export default function EquipSelect(props: {
           const mainEquip = mainEquips[index];
           return (<Fragment key={`${index}-main-select`}>
             {mainEquip && (
-              <div className="select-panel main-select">
-                <div className="text-large">{mainEquip.id}</div>
-                <div className="select-description">{mainEquip.description}</div>
-                <button onClick={() => setEquipSelected(mainEquip.id)}>{`Use`}</button>
-              </div>
+              <EquipSelectPanel
+                equip={mainEquip}
+                isTopBottom={false}
+                setEquipSelected={setEquipSelected}
+                battleState={battleState}
+                toCommand={toCommand}
+              />
             )}
             {!mainEquip && (
               <div className="select-panel main-select select-empty">
@@ -68,11 +73,13 @@ export default function EquipSelect(props: {
       </div>
 
       {bottomEquip && (
-        <div className="select-panel top-bottom-select">
-          <div className="text-large">{bottomEquip.id}</div>
-          <div className="select-description">{bottomEquip.description}</div>
-          <button onClick={() => setEquipSelected(bottomEquip.id)}>{`Use`}</button>
-        </div>
+        <EquipSelectPanel
+          equip={bottomEquip}
+          isTopBottom={true}
+          setEquipSelected={setEquipSelected}
+          battleState={battleState}
+          toCommand={toCommand}
+        />
       )}
       {!bottomEquip && (
         <div className="select-panel select-empty top-bottom-select">
@@ -82,4 +89,32 @@ export default function EquipSelect(props: {
 
     </section>
   )
+};
+
+function EquipSelectPanel(props: {
+  equip: Equipment,
+  isTopBottom: boolean,
+  setEquipSelected: (equipment: string) => void,
+  battleState: BattleState,
+  toCommand: string
+}) {
+  const { equip, isTopBottom, setEquipSelected, battleState, toCommand } = props;
+
+  const className = useMemo(() => (
+    isTopBottom ? "select-panel top-bottom-select" : "select-panel main-select"
+  ), [isTopBottom]);
+
+  const disabled = useMemo(() => (
+    equip.getCanUse ? !(equip.getCanUse({ battleState, userId: toCommand })) : false
+  ), [equip, battleState]);
+
+  return (
+    <div className={className}>
+      <div className="text-large">{equip.id}</div>
+      <div className="select-description">{equip.description}</div>
+      <button onClick={() => setEquipSelected(equip.id)} disabled={disabled}>
+        {`Use`}
+      </button>
+    </div>
+  );
 };
