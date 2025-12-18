b516ee132f7a088f48e2fa124655c72f264ecae8
Caleb Matthews <calebjmatthews@gmail.com>
2025-12-18 13:30:36 -0600

    work: flying snake ball sprite

--- client/components/OccupantSprite/OccupantSprite.tsx
+++ client/components/OccupantSprite/OccupantSprite.tsx
@@ -20,6 +20,7 @@ export default function OccupantSprite(props: {
     [CHC.JAVALIN]: { src: "/public/sprites/javalin.png", width: 13, height: 28 },
     [CHC.RAIDER]: { src: "/public/sprites/raider.png", width: 11, height: 25 },
     [CHC.FLYING_SNAKE]: { src: "/public/sprites/flying_snake.png", width: 16, height: 16 },
+    [CHC.FLYING_SNAKE_BALL]: { src: "/public/sprites/flying_snake_ball.png", width: 23, height: 25 },
     [OBK.BOULDER]: { src: "/public/sprites/rock.png", width: 17, height: 19 },
   };
   let sprite = { src: "/public/sprites/unknown.png", width: 15, height: 19 };
