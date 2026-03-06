ae19389e5a2b2d28b60bda5ff127f652915b1fdd
Caleb Matthews <calebjmatthews@gmail.com>
2026-03-06 10:12:58 -0600

    work: getEquipmentName uses enchantment name, not id

--- client/functions/getEquipmentName.ts
+++ client/functions/getEquipmentName.ts
@@ -1,22 +1,27 @@
 import type EquipmentPiece from "@common/models/equipmentPiece";
 import type RichText from "@common/models/richText";
 import equipments from "@common/instances/equipments";
+import enchantments from '@common/instances/enchantments';
 
 const getEquipmentName = (piece: EquipmentPiece): RichText | string => {
   const equipment = equipments[piece.equipmentId];
   if (!equipment) return piece.equipmentId;
 
-  const nameFull = [
-    ...(piece.enchantments ?? []),
-    equipment.name ?? equipment.id
-  ].join(" ");
+  const equipmentName = equipment.name ?? equipment.id;
+  if ((piece.enchantments ?? []).length === 0) return equipmentName;
 
-  if ((piece.enchantments ?? []).length === 0) return nameFull;
+  const namePieces = [
+    ...(piece.enchantments ?? []).map((enchantmentId) => {
+      const enchantment = enchantments[enchantmentId];
+      return enchantment?.name ?? enchantmentId
+    }),
+    equipmentName
+  ];
 
   return {
     tag: 'span',
     props: { style: { color: "var(--c-red)" } },
-    contents: [nameFull]
+    contents: [namePieces.join(" ")]
   };
 };
 
