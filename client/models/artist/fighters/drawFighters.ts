4459ac1d1f4fbffd7524525b4b2fd7405d84f380
Caleb Matthews <calebjmatthews@gmail.com>
2026-05-11 13:01:40 -0500

    work: separate initFighter into its own function

--- client/models/artist/fighters/drawFighters.ts
+++ client/models/artist/fighters/drawFighters.ts
@@ -1,3 +1,5 @@
+import * as PIXI from 'pixi.js';
+
 import type Artist from "../artist";
 import type Fighter from "@common/models/fighter";
 import fighterToLayeredAnimated from '@client/functions/artist/fighterToLayeredAnimated';
@@ -16,45 +18,56 @@ const drawFighters = (args: {
   const fightersArray = Object.values(fighters);
   fightersArray.forEach((fighter, index) => {
     if (center && index > 1) return;
-    const layeredAnimated = fighterToLayeredAnimated(fighter);
-
-    // Init fighter
+    
     if (!pixiContainers[fighter.id]) {
-      artist.layeredAnimateds[fighter.id] = layeredAnimated;
-      pixiContainers[fighter.id] = layeredAnimated.pixiContainer;
-      const scale = center ? artist.pixelScale * 1.5 : artist.pixelScale;
-      layeredAnimated.pixiContainer.scale = scale;
-
-      const firstChild = layeredAnimated.pixiContainer.children[0];
-      if (!firstChild) throw Error('Missing first child in drawFighters');
-
-      let position = { x: -1000, y: -1000 };
-
-      if (center) {
-        position = getPosition({
-          sprite: firstChild,
-          artist,
-          gravity: 'center'
-        });
-      }
-      else {
-        const spot = pixiContainers[`spot-${fighter.coords[0]}-${fighter.coords[1]}`];
-        if (!spot) return;
-
-        const spotMiddleX = spot.x + (spot.width / 2);
-        const bottomPadding = (2 * artist.pixelScale);
-        const spotBottomY = (spot.y + spot.height) - bottomPadding;
-
-        position = {
-          x: Math.round(spotMiddleX - (layeredAnimated.pixiContainer.width / 2)),
-          y: Math.round(spotBottomY - (layeredAnimated.pixiContainer.height))
-        };
-      }
-      
-      layeredAnimated.pixiContainer.position = position;
-      pixiApp.stage.addChild(layeredAnimated.pixiContainer);
+      initFighter({ artist, fighter, pixiApp, pixiContainers, center });
     };
   });
 };
+
+const initFighter = (args: {
+  artist: Artist,
+  fighter: Fighter,
+  pixiApp: PIXI.Application,
+  pixiContainers: { [id: string]: PIXI.Container<PIXI.ContainerChild> },
+  center?: boolean
+}) => {
+  const { artist, fighter, pixiApp, pixiContainers, center } = args;
+  const layeredAnimated = fighterToLayeredAnimated(fighter);
+
+  artist.layeredAnimateds[fighter.id] = layeredAnimated;
+  pixiContainers[fighter.id] = layeredAnimated.pixiContainer;
+  const scale = center ? artist.pixelScale * 1.5 : artist.pixelScale;
+  layeredAnimated.pixiContainer.scale = scale;
+
+  const firstChild = layeredAnimated.pixiContainer.children[0];
+  if (!firstChild) throw Error('Missing first child in drawFighters');
+
+  let position = { x: -1000, y: -1000 };
+
+  if (center) {
+    position = getPosition({
+      sprite: firstChild,
+      artist,
+      gravity: 'center'
+    });
+  }
+  else {
+    const spot = pixiContainers[`spot-${fighter.coords[0]}-${fighter.coords[1]}`];
+    if (!spot) return;
+
+    const spotMiddleX = spot.x + (spot.width / 2);
+    const bottomPadding = (2 * artist.pixelScale);
+    const spotBottomY = (spot.y + spot.height) - bottomPadding;
+
+    position = {
+      x: Math.round(spotMiddleX - (layeredAnimated.pixiContainer.width / 2)),
+      y: Math.round(spotBottomY - (layeredAnimated.pixiContainer.height))
+    };
+  }
+  
+  layeredAnimated.pixiContainer.position = position;
+  pixiApp.stage.addChild(layeredAnimated.pixiContainer);
+}
 
 export default drawFighters;
