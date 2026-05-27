1de1d01fd4435a082ba94536b17742166ade34c0
Caleb Matthews <calebjmatthews@gmail.com>
2026-05-27 16:28:18 -0500

    feat: character debug renders an entire battlefield

--- client/components/Debug/DebugCharacter.tsx
+++ client/components/Debug/DebugCharacter.tsx
@@ -3,6 +3,8 @@ import { useOutletContext } from "react-router";
 
 import type OutletContext from "@client/models/outlet_context";
 import type Artist from "@client/models/artist/artist";
+import type BattleState from "@common/models/battleState";
+import { battleStateEmpty } from "@common/models/battleState";
 import Fighter from "@common/models/fighter";
 import { characterClasses } from "@common/instances/character_classes";
 import { genId } from "@common/functions/utils/random";
@@ -14,17 +16,32 @@ const LAS = LAYERED_ANIMATED_STATES;
 const PIXI_CHECK_MAX_ATTEMPTS = 1000;
 const PIXI_CHECK_INTERVAL = 10;
 
-const getFighterInitial = () => {
+const getBattleStateInitial = (): BattleState => {
   const raiderClass = characterClasses[CHARACTER_CLASSES.RAIDER];
-  if (!raiderClass) throw Error('Class missing in getFighterInitial.');
-  return raiderClass.toFighter({
-    id: 'test',
-    name: 'Test',
-    ownedBy: 'testUser',
-    controlledBy: 'testUser',
-    side: 'A',
-    coords: [2, 2]
-  });
+  const javalinClass = characterClasses[CHARACTER_CLASSES.JAVALIN];
+  if (!raiderClass || !javalinClass) throw Error('Classes missing in getBattleStateInitial.');
+
+  return {
+    ...battleStateEmpty,
+    fighters: {
+      ['test']: raiderClass.toFighter({
+        id: 'test',
+        name: 'Test',
+        ownedBy: 'testUser',
+        controlledBy: 'testUser',
+        side: 'A',
+        coords: [3, 2]
+      }),
+      ['foe']: javalinClass.toFighter({
+        id: 'foe',
+        name: 'Test',
+        ownedBy: 'testUser',
+        controlledBy: 'testUser',
+        side: 'B',
+        coords: [6, 2]
+      }),
+    }
+  };
 };
 
 export default function DebugCharacter() {
@@ -33,15 +50,17 @@ export default function DebugCharacter() {
 
   const [state, setState] = useState('clean');
   const [lasState, setLasState] = useState<string>(LAS.RESTING);
-  const [fighter, setFighter] = useState(getFighterInitial());
+  const [battleState, setBattleState] = useState(getBattleStateInitial());
+  const [showColumns, setShowColumns] = useState({ 'state': true, 'events': true, 'equipment': true });
 
   useEffect(() => {
+    const artist = artistRef.current;
     if ((state === 'clean' || state.includes('re-clean')) && artistRef.current.pixiInitialized) {
       setState('ready');
+      artist.drawSpots(battleState);
       artistRef.current.drawBackground(ADVENTURE_KINDS.PRISMATIC_FALLS);
       // artistRef.current.drawBackground(`white.png`);
-      
-      artistRef.current.drawFighters({ ['test']: fighter }, true);
+      artist.drawFighters(battleState.fighters);
     }
     else if (state === 'clean' || state.includes('re-clean')) {
       let attempts = parseInt(state.replace('re-clean', ''));
@@ -63,8 +82,9 @@ export default function DebugCharacter() {
     setLasState(tLasState);
   };
 
-  const equipClick = (args: { fighter: Fighter, equipId: string, artist: Artist }) => {
+  const equipClick = (args: { fighter?: Fighter, equipId: string, artist: Artist }) => {
     const { fighter, equipId, artist } = args;
+    if (!fighter) return;
     const nextFighter = new Fighter(fighter);
     const equipNotPresent = (fighter.equipped.filter((e) => e.equipmentId === equipId).length === 0);
     if (equipNotPresent) {
@@ -76,8 +96,18 @@ export default function DebugCharacter() {
       nextFighter.equipped = fighter.equipped.filter((e) => e.equipmentId !== equipId);
     };
 
-    setFighter(nextFighter);
+    setBattleState((battleStateLast) => ({
+      ...battleStateLast,
+      fighters: {
+        ...battleStateLast.fighters,
+        ['test']: nextFighter
+      }
+    }));
     artist.drawFighters({ 'test': nextFighter });
+  };
+
+  const columnToggleClick = (column: 'state' | 'events' | 'equipment') => {
+    setShowColumns((showColumnsLast) => ({ ...showColumnsLast, [column] : !showColumnsLast[column] }));
   };
 
   if (state !== 'ready') return null;
@@ -86,27 +116,65 @@ export default function DebugCharacter() {
     <section id="debug-character">
       <section className="debug-buttons">
         <section className="debug-buttons-column">
-          <span className="text-white">State</span>
-          {LAYERED_ANIMATED_STATES_DEBUG.map((tLasState) => (
-            <button
-              key={tLasState}
-              className={tLasState === lasState ? 'is-selected' : ''}
-              onClick={() => lasStateClick(tLasState)}
-            >
-              {tLasState}
-            </button>
-          ))}
+          <div>
+            <span className="text-white">
+              <button onClick={() => columnToggleClick('state')}>
+                {showColumns['state'] && `v`}
+                {!showColumns['state'] && `<`}
+              </button>
+              State
+            </span>
+            {showColumns['state'] && LAYERED_ANIMATED_STATES_DEBUG.map((tLasState) => (
+              <button
+                key={tLasState}
+                className={tLasState === lasState ? 'is-selected' : ''}
+                onClick={() => lasStateClick(tLasState)}
+              >
+                {tLasState}
+              </button>
+            ))}
+          </div>
+          <div>
+            <span className="text-white">
+              <button onClick={() => columnToggleClick('events')}>
+                {showColumns['events'] && `v`}
+                {!showColumns['events'] && `<`}
+              </button>
+              Events
+            </span>
+            {showColumns['events'] && ['test0', 'test1', 'test2'].map((tLasState) => (
+              <button
+                key={tLasState}
+                className={tLasState === lasState ? 'is-selected' : ''}
+                onClick={() => lasStateClick(tLasState)}
+              >
+                {tLasState}
+              </button>
+            ))}
+          </div>
         </section>
         <section className="debug-buttons-column">
-          <span className="text-white">Equipment</span>
-          {EQUIPMENTS_ALL_SPRITE.map((equipId) => (
-            <button
-              key={equipId}
-              onClick={() => equipClick({ fighter, equipId, artist: artistRef.current })}
-            >
-              {equipId}
-            </button>
-          ))}
+          <div>
+            <span className="text-white">
+              <button onClick={() => columnToggleClick('equipment')}>
+                {showColumns['equipment'] && `v`}
+                {!showColumns['equipment'] && `<`}
+              </button>
+              Equipment
+            </span>
+            {showColumns['equipment'] && EQUIPMENTS_ALL_SPRITE.map((equipId) => (
+              <button
+                key={equipId}
+                onClick={() => equipClick({
+                  fighter: battleState.fighters['test'],
+                  equipId,
+                  artist: artistRef.current
+                })}
+              >
+                {equipId}
+              </button>
+            ))}
+          </div>
         </section>
       </section>
     </section>
