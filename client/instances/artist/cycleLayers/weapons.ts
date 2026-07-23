ef4d30f340cba0c41566774c1258888ea48e98fc
Caleb Matthews <calebjmatthews@gmail.com>
2026-07-23 12:16:48 -0500

    work: two additional ax sprites

--- client/instances/artist/cycleLayers/weapons.ts
+++ client/instances/artist/cycleLayers/weapons.ts
@@ -12,25 +12,44 @@ const cycleLayersWeapons: { [name: string] : CycleLayer } = {
     layers: { ...framesOneNameToCycles({
       spriteName: 'flintlet.png',
       frames: {
-        [FRN.DEFAULT]: {
-          opacity: 0
-        },
-        [FRN.ONE_LOWER]: {
-          opacity: 0
-        },
-        [FRN.CLENCHING]: {
-          offset: { x: 8, y: 1 }
-        },
-        [FRN.SWINGING_0]: {
-          offset: { x: 10, y: -6 }
-        },
-        [FRN.SWINGING_1]: {
-          offset: { x: 8, y: -4 }
-        },
-        [FRN.SWINGING_2]: {
-          offset: { x: 14, y: 27 },
-          angle: 180
-        }
+        [FRN.DEFAULT]: { opacity: 0 },
+        [FRN.ONE_LOWER]: { opacity: 0 },
+        [FRN.CLENCHING]: { offset: { x: 8, y: 1 } },
+        [FRN.SWINGING_0]: { offset: { x: 10, y: -6 } },
+        [FRN.SWINGING_1]: { offset: { x: 8, y: -4 } },
+        [FRN.SWINGING_2]: { offset: { x: 14, y: 27 }, angle: 180 }
+      }}),
+    },
+    zIndex: ARTIST_Z_INDECES.MAIN
+  },
+  [EQUIPMENTS.REVEL]: {
+    id: EQUIPMENTS.REVEL,
+    slot: CYCLE_LAYER_SLOTS.MAIN,
+    layers: { ...framesOneNameToCycles({
+      spriteName: 'claymore.png',
+      frames: {
+        [FRN.DEFAULT]: { opacity: 0 },
+        [FRN.ONE_LOWER]: { opacity: 0 },
+        [FRN.CLENCHING]: { offset: { x: 7, y: 1 } },
+        [FRN.SWINGING_0]: { offset: { x: 9, y: -6 } },
+        [FRN.SWINGING_1]: { offset: { x: 7, y: -4 } },
+        [FRN.SWINGING_2]: { offset: { x: 15, y: 27 }, angle: 180 }
+      }}),
+    },
+    zIndex: ARTIST_Z_INDECES.MAIN
+  },
+  [EQUIPMENTS.CRESCENT]: {
+    id: EQUIPMENTS.REVEL,
+    slot: CYCLE_LAYER_SLOTS.MAIN,
+    layers: { ...framesOneNameToCycles({
+      spriteName: 'crescent.png',
+      frames: {
+        [FRN.DEFAULT]: { opacity: 0 },
+        [FRN.ONE_LOWER]: { opacity: 0 },
+        [FRN.CLENCHING]: { offset: { x: 6, y: -2 } },
+        [FRN.SWINGING_0]: { offset: { x: 8, y: -9 } },
+        [FRN.SWINGING_1]: { offset: { x: 6, y: -7 } },
+        [FRN.SWINGING_2]: { offset: { x: 15, y: 30 }, angle: 180 }
       }}),
     },
     zIndex: ARTIST_Z_INDECES.MAIN
@@ -41,30 +60,13 @@ const cycleLayersWeapons: { [name: string] : CycleLayer } = {
     layers: { ...framesOneNameToCycles({
       spriteName: 'swallow.png',
       frames: {
-        [FRN.DEFAULT]: {
-          opacity: 0
-        },
-        [FRN.ONE_LOWER]: {
-          opacity: 0
-        },
-        [FRN.CLENCHING]: {
-          offset: { x: -10, y: 17 },
-          angle: 270
-        },
-        [FRN.WALKING_1]: {
-          offset: { x: -8, y: 19 },
-          angle: 270
-        },
-        [FRN.SWINGING_0]: {
-          offset: { x: 5, y: -10}
-        },
-        [FRN.SWINGING_1]: {
-          offset: { x: 7, y: -8 }
-        },
-        [FRN.SWINGING_2]: {
-          offset: { x: 0, y: 0 },
-          opacity: 0
-        }
+        [FRN.DEFAULT]: { opacity: 0 },
+        [FRN.ONE_LOWER]: { opacity: 0 },
+        [FRN.CLENCHING]: { offset: { x: -10, y: 17 }, angle: 270 },
+        [FRN.WALKING_1]: { offset: { x: -8, y: 19 }, angle: 270 },
+        [FRN.SWINGING_0]: { offset: { x: 5, y: -10} },
+        [FRN.SWINGING_1]: { offset: { x: 7, y: -8 } },
+        [FRN.SWINGING_2]: { offset: { x: 0, y: 0 }, opacity: 0 }
       }, throwingOnly: true }),
     },
     zIndex: ARTIST_Z_INDECES.MAIN
@@ -82,7 +84,7 @@ const cycleLayersWeapons: { [name: string] : CycleLayer } = {
         [FRN.SWINGING_0]: { opacity: 0 },
         [FRN.SWINGING_1]: { opacity: 0 },
         [FRN.SWINGING_2]: { opacity: 0 }
-      }, throwingOnly: true }),
+      }}),
     },
     zIndex: ARTIST_Z_INDECES.MAIN
   },
