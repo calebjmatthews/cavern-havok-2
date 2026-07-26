dbef40f031ab4f1730062a8054bbbee221d170be
Caleb Matthews <calebjmatthews@gmail.com>
2026-07-26 16:56:15 -0500

    feat: targetSprite when selecting occupant

--- client/models/artist/occupants/occupantHighlight.ts
+++ client/models/artist/occupants/occupantHighlight.ts
@@ -1,6 +1,13 @@
+import * as PIXI from 'pixi.js';
+
 import type Artist from "../artist";
 import Animation from "../animation";
+import { genId } from '@common/functions/utils/random';
 import { ANIMATION_TYPES } from "@client/enums";
+import { ANIMATION_SPEED } from '@common/constants';
+import { ARTIST_Z_INDECES } from '@common/enums';
+
+const TARGET_SPRITE_DURATION = 300;
 
 const occupantHighlight = (artist: Artist, occupantId: string) => {
   artist.occupantsUnhighlightAny();
@@ -10,6 +17,27 @@ const occupantHighlight = (artist: Artist, occupantId: string) => {
     targets: occupantId,
     infinite: true
   }));
+
+  const container = artist.pixiChildrenRef.current[occupantId];
+  const firstChildOrContainer = container?.children[0] ?? container;
+  if (!container || !firstChildOrContainer) return;
+  const textures = ['spot_target0.png', 'spot_target1.png'].map((path) => PIXI.Texture.from(path));
+  const targetSprite = new PIXI.AnimatedSprite(textures);
+  targetSprite.animationSpeed = ANIMATION_SPEED * 1.5;
+  targetSprite.zIndex = ARTIST_Z_INDECES.ANIMATED_SPRITE_EFFECTS;
+  targetSprite.loop = false;
+  targetSprite.position = {
+    x: Math.round((firstChildOrContainer.width / 2) - (targetSprite.width / 2)),
+    y: Math.round((firstChildOrContainer.height / 2) - (targetSprite.height / 2))
+  };
+  const targetSpriteId = genId();
+  artist.pixiChildrenRef.current[targetSpriteId] = targetSprite;
+  container.addChild(targetSprite);
+  targetSprite.play()
+  setTimeout(() => {
+    container.removeChild(targetSprite);
+    delete artist.pixiChildrenRef.current[targetSpriteId];
+  }, TARGET_SPRITE_DURATION);
 };
 
 export default occupantHighlight;
